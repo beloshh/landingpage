f7accb62581a52684746b98022018405c6de4dc4
badanga ishak <badangabello@gmail.com>
2020-12-09 02:46:41 -0800

    Updates

---
+++ precache-manifest.02f134e133f0a763f2fe4443cd4f94e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5d962c791fa58ddf035796e594a964",
+    "url": "/landingpage/index.html"
+  },
+  {
+    "revision": "a01279483bb64267d37c",
+    "url": "/landingpage/static/css/main.64c4377c.chunk.css"
+  },
+  {
+    "revision": "a2b505d657257bbc7383",
+    "url": "/landingpage/static/js/2.02d32bdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landingpage/static/js/2.02d32bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01279483bb64267d37c",
+    "url": "/landingpage/static/js/main.a200b674.chunk.js"
+  },
+  {
+    "revision": "a940d800f804626883ab",
+    "url": "/landingpage/static/js/runtime-main.4bd23e60.js"
+  },
+  {
+    "revision": "ff14250ea083647d87dcd9a91d656f94",
+    "url": "/landingpage/static/media/bg-curve-desktop.ff14250e.svg"
+  },
+  {
+    "revision": "e9fdc10442430eaa6f39fde1ece21ac0",
+    "url": "/landingpage/static/media/icon-arrow.e9fdc104.svg"
+  },
+  {
+    "revision": "f0f5b24aa6e8f6c57312f24da96ebf6d",
+    "url": "/landingpage/static/media/icon-email.f0f5b24a.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/landingpage/static/media/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "a51e688006d6bdc00db7e8fb4f923081",
+    "url": "/landingpage/static/media/icon-quotes.a51e6880.svg"
+  },
+  {
+    "revision": "b54a697161d657d5e1a154b7e589fb65",
+    "url": "/landingpage/static/media/illustration-1.b54a6971.svg"
+  },
+  {
+    "revision": "13d8305b15e4b62000c2e2c756b5e70c",
+    "url": "/landingpage/static/media/illustration-2.13d8305b.svg"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/landingpage/static/media/logo-footer.c71bf7bf.svg"
+  },
+  {
+    "revision": "0786a6202db9d6278c6b043c53bcc3ad",
+    "url": "/landingpage/static/media/logo.0786a620.svg"
+  }
+]);
